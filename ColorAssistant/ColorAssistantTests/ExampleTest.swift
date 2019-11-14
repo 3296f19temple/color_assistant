edde82efbf177a3fd54ae604d4facee3d05ceb97
Ian Applebaum <Ian.tyler@electricbluefish.com>
2019-11-14 18:34:31 -0500

    Added where to find more info on unit testing in swift

--- ColorAssistant/ColorAssistantTests/ExampleTest.swift
+++ ColorAssistant/ColorAssistantTests/ExampleTest.swift
@@ -18,7 +18,9 @@ class ExampleTest: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+/*
+	For information on Unit Testing in Swift I found this website https://www.swiftbysundell.com/basics/unit-testing/
+*/
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.

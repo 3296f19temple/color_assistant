68f27f68267e6dc6fdb24db4183f27f7bd351e94
Ian Applebaum <Ian.tyler@electricbluefish.com>
2019-11-14 18:17:34 -0500

    added an example of unit testing for the project

---
+++ ColorAssistant/ColorAssistant/Example.swift
@@ -0,0 +1,21 @@
+//
+//  Example.swift
+//  ColorAssistant
+//
+//  Created by Ian Applebaum on 11/14/19.
+//  Copyright © 2019 Likhon Gomes. All rights reserved.
+//
+
+import Foundation
+class Example {
+	private var exampleValue: Int
+	
+	init() {
+		self.exampleValue = 10
+	}
+	
+	func getExampleVal() -> Int {
+		return exampleValue
+	}
+	
+}

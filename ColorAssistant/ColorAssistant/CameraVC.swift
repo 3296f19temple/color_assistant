d6421927befde90c0428e64123404beb6d08ef20
likhongomes <tug60499@temple.edu>
2019-11-22 12:53:28 -0500

    custom camera working copy

---
+++ ColorAssistant/ColorAssistant/CameraVC.swift
@@ -0,0 +1,129 @@
+//
+//  CustomCamera.swift
+//  ColorAssistant
+//
+//  Created by Likhon Gomes on 11/15/19.
+//  Copyright © 2019 Likhon Gomes. All rights reserved.
+//
+
+import UIKit
+
+import AVFoundation
+
+class CameraVC: UIViewController, AVCapturePhotoCaptureDelegate {
+
+    var previewView = UIView()
+    var captureImageView = UIImageView()
+    var takePhotoButton = UIButton()
+    
+    var captureSession = AVCaptureSession()
+    var stillImageOutput = AVCapturePhotoOutput()
+    var videoPreviewLayer = AVCaptureVideoPreviewLayer()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .hd4K3840x2160
+        previewViewSetup()
+        takePhotoButtonSetup()
+        captureImageViewSetup()
+        guard let backCamera = AVCaptureDevice.default(for: .video) else {
+            print("Unable to access back Camera")
+            return
+        }
+        
+        var input:AVCaptureDeviceInput!
+        
+        do{
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error {
+            print("Error: Unable to initialize back camera: \(error.localizedDescription)")
+        }
+        
+        stillImageOutput = AVCapturePhotoOutput()
+        
+        if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+            captureSession.addInput(input)
+            captureSession.addOutput(stillImageOutput)
+            setupLivePreview()
+        }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession.startRunning()
+        }
+        
+        DispatchQueue.main.async {
+            self.videoPreviewLayer.frame = self.previewView.bounds
+        }
+    }
+    
+    func setupLivePreview() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = .resizeAspect
+        previewView.layer.addSublayer(videoPreviewLayer)
+    }
+    
+    func previewViewSetup() {
+        view.addSubview(previewView)
+        previewView.translatesAutoresizingMaskIntoConstraints = false
+        previewView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        previewView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        previewView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        previewView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        previewView.backgroundColor = .blue
+    }
+    
+    func takePhotoButtonSetup() {
+        view.addSubview(takePhotoButton)
+        takePhotoButton.translatesAutoresizingMaskIntoConstraints = false
+        takePhotoButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        takePhotoButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        takePhotoButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        takePhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        takePhotoButton.addTarget(self, action: #selector(takePhotoButtonTapped), for: .touchUpInside)
+        takePhotoButton.backgroundColor = .red
+        takePhotoButton.setTitle("Capture", for: .normal)
+        
+    }
+    
+    @objc func takePhotoButtonTapped() {
+        print("Take photo tapped")
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        guard let imageData = photo.fileDataRepresentation() else {
+            return
+        }
+        
+        let image = UIImage(data: imageData)
+        captureImageView.image = image
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
+    
+    func captureImageViewSetup() {
+        view.addSubview(captureImageView)
+        captureImageView.translatesAutoresizingMaskIntoConstraints = false
+        captureImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        captureImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        captureImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        captureImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+    }
+    
+
+}
+

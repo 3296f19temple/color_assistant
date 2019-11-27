f2c4ffca91b859f5cf60adff412eec2afdb9444a
Ian Applebaum <Ian.tyler@electricbluefish.com>
2019-11-27 01:42:29 -0500

    remapped back to FirstView again

--- ColorAssistant/ColorAssistant/CameraVC.swift
+++ ColorAssistant/ColorAssistant/CameraVC.swift
@@ -106,7 +106,7 @@ class CameraVC: UIViewController, AVCapturePhotoCaptureDelegate {
         }
         
         let image = UIImage(data: imageData)
-        let vc = OutputVC()
+        let vc = FirstView()
         vc.outputImage = image!
 		vc.img = image!
 		if #available(iOS 13, *) {

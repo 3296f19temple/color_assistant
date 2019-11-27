c991836de72da0460d81f9af211a649eeee370b7
Ian Applebaum <Ian.tyler@electricbluefish.com>
2019-11-26 21:53:53 -0500

    added compatibility for iPhone 5s. Also added a package that provides the detail of device hardware. This could allow us to select what resolution the camera is set to based on device. Also added a gesture to dismiss card view on iOS 12

---
+++ ColorAssistant/ColorAssistant/CompatabilityCheck.swift
@@ -0,0 +1,22 @@
+//
+//  CompatabilityCheck.swift
+//  ColorAssistant
+//
+//  Created by Ian Applebaum on 11/26/19.
+//  Copyright © 2019 Likhon Gomes. All rights reserved.
+//
+
+import Foundation
+import DeviceInfo
+class CompatabilityCheck {
+	func check() -> Bool {
+//		switch DeviceInformation.modelName {
+//		case "iPad Pro (12.9-inch)":
+//			return false
+//		default:
+//			return true
+//		}
+		return true
+	}
+	
+}

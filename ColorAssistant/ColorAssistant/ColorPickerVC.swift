20551ca2c527e9eb0e0eef68f42c1ba6e86acb3e
likhongomes <tug60499@temple.edu>
2019-12-04 22:54:00 -0500

    wip

--- ColorAssistant/ColorAssistant/ColorPickerVC.swift
+++ ColorAssistant/ColorAssistant/ColorPickerVC.swift
@@ -11,11 +11,27 @@ import UIKit
 class ColorPickerVC: UIViewController {
     
     let imageView = UIImageView()
-
+    var tapGestureRecognizer = UITapGestureRecognizer()
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageViewSetup()
+        
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("tap:"))
+        tap.numberOfTapsRequired = 1
 
+        view.addGestureRecognizer(tap)
  
+    }
+    
+    func tap(sender:UITapGestureRecognizer){
+
+        if sender.state == .ended {
+
+            var touchLocation: CGPoint = sender.location(in: sender.view)
+            print(touchLocation.x, touchLocation.y)
+            //touchLocation = self.convertPointFromView(touchLocation)
+
+        }
     }
     
     func imageViewSetup() {
@@ -25,6 +41,14 @@ class ColorPickerVC: UIViewController {
         imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        imageView.image = UIImage(named: "sampleImage")
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapGestureRecognizerTapped() {
+        print("hello")
+        //print(sender.location(in: imageView).x)
     }
     
 
